9abf48186c47fc87ceb2c760ed4b19b1cca3d9be
mielnikmagdalena <mielnikmagdalena@gmail.com>
2023-08-19 19:38:29 +0200

    the latest version of React Homework 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import Profile from './Profile.jsx';
+import Profile from './Profile/Profile.jsx';
 import user from './user.json';
-import Statistics from './Statistics.jsx';
+import Statistics from './Statistics/Statistics.jsx';
 import data from './data.json';
-import FriendList from './FriendList.jsx';
+import FriendList from './FriendList/FriendList.jsx';
 import friends from './friends.json';
 import transactions from './transactions.json';
-import TransactionHistory from './TransactionHistory.jsx';
+import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
 
 export const App = () => {
   return (
